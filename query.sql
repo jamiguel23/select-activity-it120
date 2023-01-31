2b21d2b68c8c33ef0198a0d55bf7aa0453fbfb4c
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-31 09:48:39 -0800

    finished class activity

---
+++ query.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT make, model from registration;
+
+SELECT DISTINCT ownertype, model from registration limit 5;
+
+SELECT DISTINCT electrification from registration;
+
+SELECT DISTINCT use, color from registration;
+
+SELECT DISTINCT electrification, state from registration;
