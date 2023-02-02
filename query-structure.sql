fef746276501912ac55c4486116e3f762f6f91b6
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-02-02 14:14:01 -0800

    added answers to query-structure file

--- query-structure.sql
+++ query-structure.sql
@@ -8,10 +8,56 @@ WHERE electrification = 'BEV (Battery Electric Vehicle)'
 GROUP BY county
 ORDER BY count DESC;
 
+-- Answer:
+--          county          | count 
+-- -------------------------+-------
+--  King                    |   732
+--  Snohomish               |   148
+--  Clark                   |    83
+--  Pierce                  |    75
+--  Kitsap                  |    37
+--  Whatcom                 |    34
+--  Spokane                 |    29
+--  Thurston                |    27
+--  Skagit                  |    17
+--  Island                  |    17
+--  Benton                  |    12
+--  Chelan                  |     9
+--  Unknown or Out of State |     8
+--  Cowlitz                 |     7
+--  Grant                   |     6
+--  Clallam                 |     6
+--  Yakima                  |     6
+--  San Juan                |     5
+--  Mason                   |     4
+--  Lewis                   |     3
+--  Grays Harbor            |     3
+--  Jefferson               |     3
+--  Franklin                |     2
+--  Klickitat               |     2
+--  Walla Walla             |     2
+--  Kittitas                |     2
+--  Skamania                |     1
+--  Whitman                 |     1
+--  Douglas                 |     1
+-- (29 rows)
+
+
+
 -- What colors are Ferraris
 SELECT DISTINCT color 
 FROM registration 
 Where make = 'FERRARI';
+
+-- Answer:  
+
+-- color       
+-- -------------------
+--  ALUMINUM / SILVER
+--  BLACK
+--  IVORY
+--  RED
+
 
 -- Number of Tesla vehicles by model
 SELECT model, count(model)
@@ -19,6 +65,17 @@ FROM registration
 WHERE Make = 'TESLA'
 GROUP BY Model
 ORDER BY count ASC;
+
+--Answer:
+--   model   | count 
+-- ----------+-------
+--  ROADSTER |     2
+--  MODEL X  |    86
+--  MODEL Y  |   120
+--  MODEL S  |   225
+--  MODEL 3  |   474
+-- (5 rows)
+
 
 -- What makes are represented in the db
 
